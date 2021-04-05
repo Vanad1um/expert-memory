d3cccc7938716af44ee49e1bcfb3c655c30dfc2b
Solomykin Ivan <solomykinbusiness@gmail.com>
2021-04-05 14:12:34 +0300

    added task5

---
+++ task5/ServletProject2/src/main/java/com/servlets/ServletGet.java
@@ -0,0 +1,18 @@
+package com.servlets;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/get")
+public class ServletGet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        String name = req.getParameter("name");
+        ServletOutputStream stream = resp.getOutputStream();
+        stream.print("Name is " + name);
+    }
+}
